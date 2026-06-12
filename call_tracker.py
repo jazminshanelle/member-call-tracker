6659f9b5fc641d5727d91342d03b2cdf17de228a
jazminshanelle <thejazminthomasproject@gmail.com>
2026-06-12 16:12:28 -0500

    Update call_tracker.py

--- call_tracker.py
+++ call_tracker.py
@@ -68,12 +68,56 @@ def create_call_record(member_id, call_type, call_minutes, resolved, call_notes,
 
     return call_record
 
+def view_analytics():
+    print("\n=== ANALYTICS DASHBOARD ===")
+
+    try:
+        with open("call_log.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print("No data available yet.")
+        return
+
+    total_calls = len(data)
+
+    resolved_calls = 0
+    total_minutes = 0
+
+    call_type_counts = {}
+
+    for record in data:
+        # resolved stats
+        if record["resolved"].lower() == "yes":
+            resolved_calls += 1
+
+        # total minutes
+        total_minutes += record["call_minutes"]
+
+        # call type count
+        ctype = record["call_type"]
+        if ctype in call_type_counts:
+            call_type_counts[ctype] += 1
+        else:
+            call_type_counts[ctype] = 1
+
+    unresolved_calls = total_calls - resolved_calls
+    avg_duration = total_minutes / total_calls if total_calls > 0 else 0
+
+    most_common_type = max(call_type_counts, key=call_type_counts.get)
+
+    print(f"Total Calls: {total_calls}")
+    print(f"Resolved Calls: {resolved_calls}")
+    print(f"Unresolved Calls: {unresolved_calls}")
+    print(f"Average Call Duration: {avg_duration:.2f} minutes")
+    print(f"Most Common Call Type: {most_common_type}")
+
 while True:
 
     print("\n=== MENU ===")
     print("1. Add new call")
     print("2. View past calls")
-    print("3. Exit")
+    print("3. View analytics")
+    print("4. Exit")
 
     choice = input("Choose an option: ")
 
@@ -84,23 +128,25 @@ while True:
         generated_summary = generate_summary(call_type, call_notes, resolved)
 
         call_record = create_call_record(
-        member_id,
-        call_type,
-        call_minutes,
-        resolved,
-        call_notes,
-        generated_summary
-)
+            member_id,
+            call_type,
+            call_minutes,
+            resolved,
+            call_notes,
+            generated_summary
+        )
+
         print(call_record)
-
         print_summary(member_id, call_type, call_minutes, resolved, call_notes, generated_summary)
-
         save_to_file(call_record)
 
     elif choice == "2":
         read_call_log()
 
     elif choice == "3":
+        view_analytics()
+
+    elif choice == "4":
         print("Exiting Member Call Tracker. Goodbye!")
         break
 
